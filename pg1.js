1a2e6929f094fbe1dfd060f26d92c4dd419fb86b
SAMUEL DENNIS DAVID <samueld.david01@gmail.com>
2025-01-31 10:38:08 +0530

    Create pg1.js
    pg1

---
+++ pg1.js
@@ -0,0 +1,20 @@
+
+const arr = [1, 2, 3, 4, 5];
+var arr1 = [4, 0, 9, 8, 2];
+let arr2 = [4, 0, 9, 8, 2];
+
+
+arr[0] = "new";
+arr1[0] = "new";
+arr2[0] = "new";
+console.log(arr)
+console.log(arr1)
+console.log(arr2)
+
+
+var x = 10;
+const y = 20;
+console.log(x)
+console.log(y)
+x = 30;
+console.log(x)

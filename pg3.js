5fd1c512c0cc2a429eb254906f4e686fcb57af26
SAMUEL DENNIS DAVID <samueld.david01@gmail.com>
2025-02-11 10:31:40 +0530

    Update pg3.js

--- pg3.js
+++ pg3.js
@@ -1,34 +1,6 @@
-//add element in array and check the number is integer or not.
-const readline = require('readline');
+const readlineSync = require('readline-sync');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// Prompt the user for input
+const userInput = readlineSync.question('Please enter a value: ');
 
-let array = [1, 2, 3, 4, 5];
-console.log('Initial Array: ', array);
-
-rl.question('Enter a number to add to the array: ', (newElement) => {
-  const num = parseInt(newElement);
-
-  if (!isNaN(num)) {
-    array.push(num);
-    console.log('Updated Array: ', array);
-
-    rl.question('Enter an index to retrieve the value from the array: ', (input) => {
-      const index = parseInt(input);
-
-      if (!isNaN(index) && index >= 0 && index < array.length) {
-        console.log(`Value at index ${index}: ${array[index]}`);
-      } else {
-        console.log('Invalid index.');
-      }
-
-      rl.close();
-    });
-  } else {
-    console.log('Invalid number entered.');
-    rl.close();
-  }
-});
+console.log(`You entered: ${userInput}`);
